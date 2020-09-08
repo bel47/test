b777c6bb16ffe72b6a0e2e1c6242e118d2159e81
Belay <tony.retta@gmail.com>
2020-09-08 15:41:23 +0300

    new commit

--- src/main/java/com/ticketing/ts/dao/RouteDAOImpl.java
+++ src/main/java/com/ticketing/ts/dao/RouteDAOImpl.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ticketing.ts.model.Passenger;
 import com.ticketing.ts.model.Route;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RouteDAOImpl implements RouteDAO {
 
 	@Autowired
@@ -19,7 +21,7 @@ public class RouteDAOImpl implements RouteDAO {
 	@Override
 	public List<Route> get() {
 		Session currentSession = entityManager.unwrap(Session.class);
-		Query query = currentSession.createQuery("from Passenger", Route.class);
+		Query query = currentSession.createQuery("from Route", Route.class);
 		List<Route> list = query.getResultList();
 		return list;
 	}
